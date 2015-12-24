b5a8cbf7ad2767415a264d3a4ca0412bb1909385
Kevin W Palmer <kevin@floatleftinteractive.com>
2015-12-24 12:17:42 -0500

    pulling in and displaying featured row data.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -2,13 +2,26 @@
 
 /* Controllers */
 
-var phonecatApp = angular.module('phonecatApp', []);
+var beyondTodayApp = angular.module('beyondTodayApp', []);
+beyondTodayApp.featuredUrl = 'http://www.ucg.org/api/v1.0/featured_media';
+beyondTodayApp.mediaUrl = 'http://www.ucg.org/api/v1.0/media/';
+beyondTodayApp.finalFeaturedUrl = "";
+beyondTodayApp.featuredIds = "";
 
-phonecatApp.controller('PhoneListCtrl', ['$scope', '$http', function($scope, $http) {
-  $http.get('http://www.ucg.org/api/v1.0/featured_media').success(function(data) {
-    $scope.phones = data;
-    console.log($scope.phones);
+beyondTodayApp.controller('FeaturedRowCtrl', ['$scope', '$http', function($scope, $http) {
+  $http.get(beyondTodayApp.featuredUrl).success(function(data) {
+
+    for(var fff in data.data){
+
+      beyondTodayApp.featuredIds += data.data[fff].id + ",";
+    }
+      beyondTodayApp.featuredIds =   beyondTodayApp.featuredIds.substring(0,  beyondTodayApp.featuredIds.length -1);
+      beyondTodayApp.finalFeaturedUrl = beyondTodayApp.mediaUrl + beyondTodayApp.featuredIds;
+
+  $http.get(beyondTodayApp.finalFeaturedUrl).success(function(data) {
+    
+      $scope.featuredRowData = data.data;
+    });
   });
 
-  $scope.orderProp = 'position';
 }]);
